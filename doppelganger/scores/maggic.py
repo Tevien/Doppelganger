2de76bc0144dfc043c5b13212f8fe7bbffe0aa87
Tevien <s.h.benson@amsterdamumc.nl>
2024-02-09 15:48:47 +0100

    add 1 year risk to maggic

--- doppelganger/scores/maggic.py
+++ doppelganger/scores/maggic.py
@@ -5,7 +5,7 @@ def calculateMAGGIC(data:pd.DataFrame, columns:Dict[str, str],
                     ) -> pd.DataFrame:
 
     '''
-    Caculated the MAGGIC score predicting 3-years death in heart failure
+    Caculated the MAGGIC score predicting 3-years and 1-year death in heart failure
     patients. Based on Pocock et al. 2013 EHJ:
     https://academic.oup.com/eurheartj/article/34/19/1404/422939
 
@@ -24,26 +24,26 @@ def calculateMAGGIC(data:pd.DataFrame, columns:Dict[str, str],
     Example `column` input:
 
     >>> maggic_columns = {
-    >>> 'sex'           : 'Female sex',  # binary 1 for female
-    >>> 'smoking'       : 'Smoker',      # binary 1 for current smoker
-    >>> 'hist_diabetes' : 'Diabetes',     # binary 1 for any diabetes
-    >>> 'hist_copd'     : 'HistoryCOPD', # binary 1 for COPD
-    >>> 'recent_hf'     : 'HF_first18M', # binary 1 HF diagnosis within the first 18M
-    >>> 'betablocker'   : 'Med_BB',      # binary 1 for BB medication
-    >>> 'ace_arb'       : 'Med_ACEi',    # binary 1 for ACE/ARB medication
-    >>> 'lvef'          : 'LV - EF (%)',
-    >>> 'nyha'          : 'NYHA class',
-    >>> 'creatinine'    : 'Creatinine (μmol/L)',
-    >>> 'bmi'           : 'BMI',
-    >>> 'sbp'           : 'systBP',
-    >>> 'age'           : 'Age (years)',
+    #>>> 'sex'           : 'Female sex',  # binary 1 for female
+    #>>> 'smoking'       : 'Smoker',      # binary 1 for current smoker
+    #>>> 'hist_diabetes' : 'Diabetes',     # binary 1 for any diabetes
+    #>>> 'hist_copd'     : 'HistoryCOPD', # binary 1 for COPD
+    #>>> 'recent_hf'     : 'HF_first18M', # binary 1 HF diagnosis within the first 18M
+    #>>> 'betablocker'   : 'Med_BB',      # binary 1 for BB medication
+    #>>> 'ace_arb'       : 'Med_ACEi',    # binary 1 for ACE/ARB medication
+    !(FROM FREE TEXT)>>> 'lvef'          : 'LV - EF (%)',
+    !(FROM FREE TEXT)>>> 'nyha'          : 'NYHA class',
+    #>>> 'creatinine'    : 'Creatinine (μmol/L)',
+    #>>> 'bmi'           : 'BMI',
+    #>>> 'sbp'           : 'systBP',
+    #>>> 'age'           : 'Age (years)',
     }
 
     Returns
     -------
     maggic : pd.DataFrame,
         a DataFrame with the MAGGIC score caculated, the 3 years risk of death
-        and an index equal to the index of `data`.
+        + 1 year risk of death and an index equal to the index of `data`.
     '''
 
     # check input
@@ -72,7 +72,7 @@ def calculateMAGGIC(data:pd.DataFrame, columns:Dict[str, str],
     COPD = 'hist_copd'; RECENT_HF = 'recent_hf'; BETAB = 'betablocker'
     ACE = 'ace_arb'; LVEF = 'lvef'; NYHA = 'nyha'; CREAT = 'creatinine'
     BMI = 'bmi'; SBP = 'sbp'; AGE='age'
-    MAGGIC='maggic'; MAGGIC_RISK3='maggic (3-years risk of death)'
+    MAGGIC='maggic'; MAGGIC_RISK3='maggic (3-years risk of death)'; MAGGIC_RISK1='maggic (1-year risk of death)'
 
     # score to risk
     DEATH_YEAR3 = {0: 0.039, 1: 0.043, 2: 0.048, 3: 0.052, 4: 0.058, 5: 0.063,
@@ -84,8 +84,18 @@ def calculateMAGGIC(data:pd.DataFrame, columns:Dict[str, str],
                    32: 0.590, 33: 0.625, 34: 0.658, 35: 0.692, 36: 0.725,
                    37: 0.756, 38: 0.787, 39: 0.815, 40: 0.842, 41: 0.866,
                    42: 0.889, 43: 0.908, 44: 0.926, 45: 0.941, 46: 0.953,
-                   47: 0.964, 48: 0.973, 49: 0.980,
+                   47: 0.964, 48: 0.973, 49: 0.980, 50: 0.985
                    }
+    DEATH_YEAR1 = {0: 0.015, 1: 0.016, 2: 0.018, 3: 0.020, 4: 0.022, 5: 0.024,
+                   6: 0.027, 7: 0.029, 8: 0.032, 9: 0.036, 10: 0.039, 11: 0.043,
+                   12: 0.048, 13: 0.052, 14: 0.058, 15: 0.063, 16: 0.070,
+                   17: 0.077, 18: 0.084, 19: 0.093, 20: 0.102, 21: 0.111,
+                   22: 0.122, 23: 0.134, 24: 0.147, 25: 0.160, 26: 0.175,
+                   27: 0.186, 28: 0.202, 29: 0.219, 30: 0.237, 31: 0.256,
+                   32: 0.292, 33: 0.316, 34: 0.342, 35: 0.369, 36: 0.398,
+                   37: 0.427, 38: 0.458, 39: 0.490, 40: 0.523, 41: 0.557,
+                   42: 0.591, 43: 0.625, 44: 0.659, 45: 0.692, 46: 0.725,
+                   47: 0.757, 48: 0.787, 49: 0.816, 50: 0.842}
 
     # ### main effects
     # Male sex
@@ -201,14 +211,16 @@ def calculateMAGGIC(data:pd.DataFrame, columns:Dict[str, str],
 
     # #### calculate risk
     death_risk3 = [*map(DEATH_YEAR3.get, maggic)]
+    death_risk1 = [*map(DEATH_YEAR1.get, maggic)]
 
     # turn into pd.DataFrame
-    new_data = pd.DataFrame({MAGGIC: maggic, MAGGIC_RISK3:death_risk3},
+    new_data = pd.DataFrame({MAGGIC: maggic, MAGGIC_RISK3:death_risk3,
+                                MAGGIC_RISK1:death_risk1},
                         index=data.index)
 
     # #### add missing values
     new_data.loc[data[list(columns.values())].isnull().any(axis=1),
-                 [MAGGIC, MAGGIC_RISK3]] = np.nan
+                 [MAGGIC, MAGGIC_RISK3, MAGGIC_RISK1]] = np.nan
 
     # ### return
     return new_data
