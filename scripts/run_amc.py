bd2679ece9a77c1e1f9fe044b5df030acb326606
Tevien <sean.h.benson@googlemail.com>
2026-01-15 01:45:04 +0100

    full pipeline in run_amc

--- scripts/run_amc.py
+++ scripts/run_amc.py
@@ -1,6 +1,7 @@
 from dpplgngr.etl.prep_dataset_tabular import ConvertLargeFiles, PreProcess, TuplesProcess
 from dpplgngr.train.sdv import SDVGen
 from dpplgngr.scores.audit import SyntheticDataAudit
+from dpplgngr.scores.privacy import PrivacyEvaluation
 
 # Try to import luigi, fallback to replacement if not available
 try:
@@ -27,7 +28,7 @@ import argparse
 # Set up the command line arguments
 parser = argparse.ArgumentParser(description='Run the AMC ETL pipeline.')
 parser.add_argument('--etlconfig', type=str, help='Path to the ETL configuration file.',
-                    default='/home/sbenson/sw/Doppelganger/config/etl_amc_v3.json')
+                    default='/home/sbenson/sw/Doppelganger/config/etl_amc_hpc_snowflake_equiv.json')
 parser.add_argument('--genconfig', type=str, help='Path to the generation configuration file.',
                     default='/home/sbenson/sw/Doppelganger/config/synth.json')
 args = parser.parse_args()
@@ -38,6 +39,8 @@ luigi.build([ConvertLargeFiles(etl_config=etl_config),
              PreProcess(etl_config=etl_config),
              TuplesProcess(etl_config=etl_config),
              SDVGen(gen_config=gen_config, etl_config=etl_config),
-             SyntheticDataAudit(gen_config=gen_config, etl_config=etl_config)
+             SyntheticDataAudit(gen_config=gen_config, etl_config=etl_config),
+             PrivacyEvaluation(gen_config=gen_config, etl_config=etl_config)
+             
              ], 
-workers=2, local_scheduler=True, no_lock=False, log_level='INFO')
+workers=1, local_scheduler=True, no_lock=False, log_level='INFO')
